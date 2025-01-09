85d3d6ecbb19611d75ab91c88a187df3b7945cc1
onkar <onkar.chendage@gmail.com>
2025-01-09 17:18:33 +0530

    modularise privileges watcher and loader

---
+++ src/services/privilege_loader.go
@@ -0,0 +1,57 @@
+package services
+
+import (
+	"aura/src/utils"
+	"sync"
+)
+
+type PrivilegeLoader struct {
+	filename                 string
+	effectivePrivilegesCache *sync.Map
+	cacheMutex               sync.Mutex
+}
+
+func NewPrivilegeLoader(filename string) *PrivilegeLoader {
+	return &PrivilegeLoader{
+		filename:                 filename,
+		effectivePrivilegesCache: &sync.Map{},
+	}
+}
+
+func (pl *PrivilegeLoader) LoadAndComputePrivileges() error {
+	privileges, err := utils.LoadPrivileges(pl.filename)
+	if err != nil {
+		return err
+	}
+	pl.computeEffectivePrivileges(privileges)
+	return nil
+}
+
+func (pl *PrivilegeLoader) computeEffectivePrivileges(privileges *utils.RolePrivileges) {
+	var newPrivileges sync.Map
+	for role := range privileges.Roles {
+		effective := utils.ComputeRolePrivilegesDFS(role, privileges, make(map[string]bool))
+		newPrivileges.Store(role, effective)
+	}
+
+	pl.cacheMutex.Lock()
+	defer pl.cacheMutex.Unlock()
+
+	pl.effectivePrivilegesCache.Range(func(key, value interface{}) bool {
+		pl.effectivePrivilegesCache.Delete(key)
+		return true
+	})
+	newPrivileges.Range(func(key, value interface{}) bool {
+		pl.effectivePrivilegesCache.Store(key, value)
+		return true
+	})
+}
+
+func (pl *PrivilegeLoader) GetEffectivePrivileges(role string) ([]string, bool) {
+
+	privileges, ok := pl.effectivePrivilegesCache.Load(role)
+	if !ok {
+		return nil, false
+	}
+	return privileges.([]string), true
+}
---
+++ src/services/privilege_watcher.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"log"
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+type FileWatcher struct {
+	filename        string
+	privilegeLoader *PrivilegeLoader
+}
+
+func NewFileWatcher(filename string) *FileWatcher {
+	privilegeLoader := NewPrivilegeLoader(filename)
+	return &FileWatcher{
+		filename:        filename,
+		privilegeLoader: privilegeLoader,
+	}
+}
+
+func (f *FileWatcher) GetEffectivePrivileges(role string) ([]string, bool) {
+	return f.privilegeLoader.GetEffectivePrivileges(role)
+}
+
+func (f *FileWatcher) GetEffectivePrivilegess() *sync.Map {
+	return f.privilegeLoader.effectivePrivilegesCache
+}
+
+func (f *FileWatcher) Start() error {
+	if err := f.privilegeLoader.LoadAndComputePrivileges(); err != nil {
+		return err
+	}
+
+	go func() {
+		if err := f.watch(); err != nil {
+			log.Fatal("error watching file:", err)
+		}
+	}()
+	return nil
+}
+
+func (f *FileWatcher) watch() error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	defer watcher.Close()
+
+	err = watcher.Add(f.filename)
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case event := <-watcher.Events:
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				if err := f.privilegeLoader.LoadAndComputePrivileges(); err != nil {
+					log.Println("error reloading privileges:", err)
+				} else {
+					log.Println("privileges reloaded successfully")
+				}
+			}
+		case err := <-watcher.Errors:
+			log.Println("watcher error:", err)
+		}
+	}
+}
