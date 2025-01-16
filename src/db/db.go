daed7dd3b77b23bd084ecc58508559e4a19e14f6
onkar <onkar.chendage@gmail.com>
2025-01-17 01:51:41 +0530

    add sqldb connection

---
+++ src/db/db.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type DB interface {
+	AssignRole(userID, role, resourceID string) error
+	RemoveRole(userID, resourceID string) error
+	GetRole(userID, resourceID string) (string, error)
+	Close() error
+}
+
+type sqlDB struct {
+	conn      *sql.DB
+	validator *validator.Validate
+}
+
+func NewDB(conn *sql.DB) DB {
+	return &sqlDB{conn: conn, validator: validator.New()}
+}
+
+func (db *sqlDB) Close() error {
+	return db.conn.Close()
+}
+
+func (db *sqlDB) validateInput(input interface{}) error {
+	if err := db.validator.Struct(input); err != nil {
+		return fmt.Errorf("invalid input: %w", err)
+	}
+	return nil
+}
