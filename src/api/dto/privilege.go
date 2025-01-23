8d408c697aa181843901a4db9dd793fa04dcc4aa
onkar <onkar.chendage@gmail.com>
2025-01-23 23:43:28 +0530

    add dto for assign privilege api

--- src/api/dto/privilege.go
+++ src/api/dto/privilege.go
@@ -10,6 +10,16 @@ type CheckPrivilegeResponse struct {
 	Allowed bool `json:"allowed"`
 }
 
+type AssignPrivilegeRequest struct {
+	User     string `json:"user"`
+	Action   string `json:"action"`
+	Resource string `json:"resource"`
+}
+
+type AssignPrivilegeResponse struct {
+	Success bool `json:"success"`
+}
+
 type ErrorResponse struct {
 	Error   string `json:"error"`
 	Code    int    `json:"code"`
