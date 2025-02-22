759accdacc52028ced0291f0b2ad938535dc3273
onkar <onkar.chendage@gmail.com>
2025-02-22 22:34:03 +0530

    add health check endpoint to router

--- src/api/router.go
+++ src/api/router.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 func NewRouter() *gin.Engine {
 	api := &API{}
 	router := gin.Default()
+	router.GET("/healthz", func(c *gin.Context) { c.JSON(200, gin.H{"status": "ok"}) })
 
 	server := NewStrictHandler(api, nil)
 
