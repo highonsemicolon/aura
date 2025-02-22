8f94c4fb61af33d63190bea6862cbcbdcada0c9c
onkar <onkar.chendage@gmail.com>
2025-02-22 07:54:24 +0530

    add basic apis

--- src/api/api.go
+++ src/api/api.go
@@ -1,15 +1,43 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+)
+
+type API struct{}
+
+func (a *API) LookupResources(ctx context.Context, x LookupResourcesRequestObject) (LookupResourcesResponseObject, error) {
+	return LookupResources200JSONResponse{}, nil
+}
+
+func (a *API) CreateRelationship(ctx context.Context, x CreateRelationshipRequestObject) (CreateRelationshipResponseObject, error) {
+	return CreateRelationship201Response{}, nil
+}
+
+func (a *API) CheckPermission(ctx context.Context, x CheckPermissionRequestObject) (CheckPermissionResponseObject, error) {
+	return CheckPermission200JSONResponse{}, nil
+}
+
+func (a *API) DeleteRelationship(ctx context.Context, x DeleteRelationshipRequestObject) (DeleteRelationshipResponseObject, error) {
+	return DeleteRelationship200Response{}, nil
+}
+
+func (a *API) ExpandRelationships(ctx context.Context, x ExpandRelationshipsRequestObject) (ExpandRelationshipsResponseObject, error) {
+	return ExpandRelationships200JSONResponse{}, nil
+}
 
 func NewApp() *gin.Engine {
-
+	api := &API{}
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, World!",
-		})
-	})
+
+	server := NewStrictHandler(api, nil)
+
+	v1 := r.Group("/api/v1")
+	{
+		RegisterHandlers(v1, server)
+	}
 
 	return r
 
