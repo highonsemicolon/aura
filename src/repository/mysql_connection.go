f6ac25680846a0f70d9a39a0e8f390b714b64f5d
onkar <onkar.chendage@gmail.com>
2025-02-23 15:58:55 +0530

    refactor InitDB function to use config struct for MySQL connection parameters

--- src/repository/mysql_connection.go
+++ src/repository/mysql_connection.go
@@ -6,20 +6,21 @@ import (
 	"log"
 
 	"github.com/go-sql-driver/mysql"
-	"github.com/highonsemicolon/aura/utils"
+	"github.com/highonsemicolon/aura/config"
+	"github.com/highonsemicolon/aura/src/utils"
 )
 
-func InitDB(dsn, CACertPath string) *sql.DB {
-	if CACertPath != "" {
-		tlsConfig, err := utils.InitTLS(CACertPath)
+func InitDB(config config.MySQL) *sql.DB {
+	if config.CAPath != "" {
+		tlsConfig, err := utils.InitTLS(config.CAPath)
 		if err != nil {
-			panic(fmt.Errorf("TLS setup failed for CA Cert '%s': %w", CACertPath, err))
+			panic(fmt.Errorf("TLS setup failed for CA Cert '%s': %w", config.CAPath, err))
 		}
 
 		mysql.RegisterTLSConfig("custom", tlsConfig)
 	}
 
-	db, err := sql.Open("mysql", dsn)
+	db, err := sql.Open("mysql", config.DSN)
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
