233acc6b2b31ea968ae30662a46300216467623b
onkar <onkar.chendage@gmail.com>
2025-02-23 22:59:53 +0530

    feat: implement transaction handling in MySQLRepository

--- src/repository/mysql_connection.go
+++ src/repository/mysql_connection.go
@@ -31,3 +31,16 @@ func InitDB(config config.MySQL) *sql.DB {
 
 	return db
 }
+
+func (r *MySQLRepository[T]) withTransaction(fn func(tx *sql.Tx) error) error {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	if err := fn(tx); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
