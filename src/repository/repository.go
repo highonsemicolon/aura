a9adfa50a765b01c91d22a17bac5a3c07fe810b7
onkar <onkar.chendage@gmail.com>
2025-02-23 11:17:36 +0530

    add generic Repository interface for CRUD operations

---
+++ src/repository/repository.go
@@ -0,0 +1,9 @@
+package repository
+
+type Repository[T any] interface {
+	GetByID(id string) (*T, error)
+	GetAll() ([]T, error)
+	Create(entity *T) error
+	Update(entity *T) error
+	Delete(id string) error
+}
