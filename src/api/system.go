edb8986e151062a485273264212c607204dbc646
onkar <onkar.chendage@gmail.com>
2025-03-18 00:10:44 +0530

    feat: restructure system info response for improved organization and clarity

--- src/api/system.go
+++ src/api/system.go
@@ -22,19 +22,25 @@ func infoHandler(c *gin.Context) {
 	uptime := time.Since(startTime).String()
 
 	info := gin.H{
-		"app_name":      appName,
-		"app_env":       os.Getenv("APP_ENV"),
-		"version":       version,
-		"commit":        commitHash,
-		"build_time":    buildTime,
-		"build_host":    buildHost,
-		"start_time":    startTime.Format(time.RFC3339),
-		"uptime":        uptime,
-		"hostname":      hostname,
-		"go_version":    runtime.Version(),
-		"num_cpu":       runtime.NumCPU(),
-		"num_goroutine": runtime.NumGoroutine(),
-		"num_cgo_call":  runtime.NumCgoCall(),
+		"name": appName,
+		"env":  os.Getenv("APP_ENV"),
+
+		"version": version,
+		"build": map[string]any{
+			"commit":     commitHash,
+			"build_time": buildTime,
+			"build_host": buildHost,
+		},
+		"deployment": map[string]any{
+			"go_version":    runtime.Version(),
+			"arch":          runtime.GOARCH,
+			"start_time":    startTime.Format(time.RFC3339),
+			"uptime":        uptime,
+			"hostname":      hostname,
+			"num_cpu":       runtime.NumCPU(),
+			"num_goroutine": runtime.NumGoroutine(),
+			"num_cgo_call":  runtime.NumCgoCall(),
+		},
 	}
 	c.JSON(200, info)
 }
