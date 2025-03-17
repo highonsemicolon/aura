21125431b1f952fe7780863bc54843227ff472ec
onkar <onkar.chendage@gmail.com>
2025-03-17 23:27:45 +0530

    feat: add system info endpoint to provide application metadata and runtime statistics

---
+++ src/api/system.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	appName    = "unknown"
+	version    = "unknown"
+	commitHash = "unknown"
+	buildTime  = "unknown"
+	buildHost  = "unknown"
+	startTime  = time.Now()
+)
+
+func infoHandler(c *gin.Context) {
+	hostname, _ := os.Hostname()
+	uptime := time.Since(startTime).String()
+
+	info := gin.H{
+		"app_name":      appName,
+		"app_env":       os.Getenv("APP_ENV"),
+		"version":       version,
+		"commit":        commitHash,
+		"build_time":    buildTime,
+		"build_host":    buildHost,
+		"start_time":    startTime,
+		"uptime":        uptime,
+		"hostname":      hostname,
+		"go_version":    runtime.Version(),
+		"num_cpu":       runtime.NumCPU(),
+		"num_goroutine": runtime.NumGoroutine(),
+		"num_cgo_call":  runtime.NumCgoCall(),
+	}
+	c.JSON(200, info)
+}
