33016501f1a32f6860974391cb68a53b0a5b0803
onkar <onkar.chendage@gmail.com>
2025-03-22 15:06:44 +0530

    feat: add server interface to mock easily

--- src/server/server.go
+++ src/server/server.go
@@ -2,33 +2,9 @@ package server
 
 import (
 	"context"
-	"net/http"
-	"time"
 )
 
-type Server struct {
-	httpServer *http.Server
-}
-
-func NewServer(addr string, handler http.Handler) *Server {
-
-	return &Server{
-		httpServer: &http.Server{
-			Addr:              addr,
-			Handler:           handler,
-			ReadTimeout:       5 * time.Second,
-			ReadHeaderTimeout: 2 * time.Second,
-			WriteTimeout:      10 * time.Second,
-			IdleTimeout:       15 * time.Second,
-			MaxHeaderBytes:    1 << 20,
-		},
-	}
-}
-
-func (s *Server) ListenAndServe() error {
-	return s.httpServer.ListenAndServe()
-}
-
-func (s *Server) Shutdown(ctx context.Context) error {
-	return s.httpServer.Shutdown(ctx)
+type HTTPServer interface {
+	ListenAndServe() error
+	Shutdown(ctx context.Context) error
 }
