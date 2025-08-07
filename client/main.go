0a24c32cc4e52632036e3923c39e90024ea1f091
onkar <onkar.chendage@gmail.com>
2025-08-07 09:04:40 +0530

    refactor: simplify tracer initialization by using telemetry package

--- client/main.go
+++ client/main.go
@@ -8,58 +8,20 @@ import (
 	"os"
 	"time"
 
+	"github.com/highonsemicolon/aura/internal/telemetry"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
-func initTracer(serviceName, endpoint string) (func(), error) {
-	ctx := context.Background()
-
-	exp, err := otlptracehttp.New(ctx,
-		otlptracehttp.WithEndpoint(endpoint),
-		otlptracehttp.WithInsecure(),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("creating OTLP exporter: %w", err)
-	}
-
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exp),
-		sdktrace.WithResource(resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceName(serviceName),
-		)),
-	)
-
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
-		propagation.TraceContext{},
-		propagation.Baggage{},
-	))
-
-	return func() {
-		_ = tp.Shutdown(ctx)
-	}, nil
-}
-
 func main() {
-	// Config
 	endpoint := os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
 	if endpoint == "" {
 		endpoint = "localhost:4318"
 	}
 
-	shutdown, err := initTracer("http-client-service", endpoint)
-	if err != nil {
-		panic(err)
-	}
-	defer shutdown()
+	telemetryShutdown := telemetry.InitTracer("http-client-service", endpoint)
+	defer telemetryShutdown()
 
 	client := http.Client{
 		Transport: otelhttp.NewTransport(http.DefaultTransport),
@@ -75,7 +37,6 @@ func main() {
 		attribute.String("client", "my-http-client"),
 	)
 
-	// Make the request to the server service
 	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080/", nil)
 
 	res, err := client.Do(req)
