3e0d09edc455a8075dbb197138c92b2ac95f449b
onkar <onkar.chendage@gmail.com>
2025-11-02 19:31:05 +0530

    refactor(config): remove reflection for database aliasing in MongoDB struct

--- services/app/internal/config/mongodb.go
+++ services/app/internal/config/mongodb.go
@@ -1,7 +1,5 @@
 package config
 
-import "reflect"
-
 const (
 	DBUsers     = "users"
 	DBOrders    = "orders"
@@ -10,27 +8,15 @@ const (
 
 type MongoDB struct {
 	URI         string `koanf:"uri" validate:"required,url"`
-	UserDB      string `koanf:"user_db" validate:"required" dbalias:"users"`
-	OrderDB     string `koanf:"order_db" validate:"required" dbalias:"orders"`
-	AnalyticsDB string `koanf:"analytics_db" validate:"required" dbalias:"analytics"`
+	UserDB      string `koanf:"user_db" validate:"required"`
+	OrderDB     string `koanf:"order_db" validate:"required"`
+	AnalyticsDB string `koanf:"analytics_db" validate:"required"`
 }
 
 func (m MongoDB) Databases() map[string]string {
-	dbs := make(map[string]string)
-	t := reflect.TypeOf(m)
-	v := reflect.ValueOf(m)
-
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		dbalias := field.Tag.Get("dbalias")
-		if dbalias == "" {
-			continue
-		}
-
-		val := v.Field(i).String()
-		if val != "" {
-			dbs[dbalias] = val
-		}
+	return map[string]string{
+		DBUsers:     m.UserDB,
+		DBOrders:    m.OrderDB,
+		DBAnalytics: m.AnalyticsDB,
 	}
-	return dbs
 }
